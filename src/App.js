bcda9cefdcb3e342e2caa730e884d895587363dc
ristep <ristepan@gmail.com>
2021-01-28 10:55:20 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link
@@ -8,24 +8,24 @@ import {
 import Home from "./pages/home";
 import About from "./pages/about";
 import Users from "./pages/users";
+import { NavLink, Nav, Navbar } from "react-bootstrap";
 
 export default function App() {
+
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar className="navbar-dark bg-primary" expand="lg">
+            <Navbar.Brand>Json API test</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <NavLink as={Link} to="/">Home</NavLink>
+                <NavLink as={Link} to="/about">About</NavLink>
+                <NavLink as={Link} to="/users">Users</NavLink>
+              </Nav>
+            </Navbar.Collapse>
+        </Navbar>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
@@ -40,6 +40,7 @@ export default function App() {
             <Home />
           </Route>
         </Switch>
+        
       </div>
     </Router>
   );
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h2>Home</h2>;
+  return <h2>Домашна страна</h2>;
 }
 
 export default Home;
