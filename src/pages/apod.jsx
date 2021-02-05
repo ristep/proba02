50217e7ee76bbba72ad26db446ed77eadaef782c
ristep <ristepan@gmail.com>
2021-02-05 01:44:35 +0100

    epic 01

--- src/pages/apod.jsx
+++ src/pages/apod.jsx
@@ -1,25 +1,57 @@
 import React, { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import ReactJson from "react-json-view";
+import { Card } from "react-bootstrap";
 
-export const Apod =  () => {
+export const Apod = () => {
   const [apod, setApod] = useState({});
-
+  const [apodList, setApodList] = useState({});
+ 
   useEffect(() => {
-    axios.get(`https://api.nasa.gov/planetary/apod?start_date=2021-01-01&end_date=2021-02-02&api_key=DEMO_KEY`)
-    .then(res => {
-      const apod = res.data;
-      setApod({ apod });
-    });
-  return () => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?start_date=2021-01-01&end_date=2021-02-02&api_key=14kQ7Rt7juZFOot4zYe0Tacjt0Z7s66H3MjEbdRU`
+      )
+      .then((res) => {
+        setApodList(res.data);
+      });
+    return () => {
       //cleanup
     };
-  },[]);
+  }, []);
 
-  return( 
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.nasa.gov/planetary/apod?api_key=14kQ7Rt7juZFOot4zYe0Tacjt0Z7s66H3MjEbdRU`
+      )
+      .then((res) => {
+        setApod(res.data);
+        // setHeaders(res);
+      });
+    return () => {
+      //cleanup
+    };
+  }, []);
+
+  return (
     <>
       <h2>Astronomy Photo of the Day</h2>
-      <ReactJson src={apod} ></ReactJson>
+      
+      {/* <Card className="bg-dark text-white">
+        <Card.Img src="" alt="Card image" />
+        <Card.ImgOverlay>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+          <Card.Text>Last updated 3 mins ago</Card.Text>
+        </Card.ImgOverlay>
+      </Card> */}
+
+      <ReactJson src={apod}></ReactJson>
+      <ReactJson src={apodList}></ReactJson>
     </>
-    );
+  );
 };
--- src/pages/epic.jsx
+++ src/pages/epic.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import ReactJson from "react-json-view";
+
 import { Button } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 
 export const Epic = () => {
   const history = useHistory();
-  
+  const [epicList, setEpicList] = useState({});
+
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/EPIC/api/natural/all?api_key=14kQ7Rt7juZFOot4zYe0Tacjt0Z7s66H3MjEbdRU`)
+    .then(res => {
+      setEpicList(res.data);
+    })  
+  },[]);
+
   const onClick = () => {
     history.push('/users');
   }
 
   return(
     <div>
-      <h1>Settings</h1>
+      <h1>EPIC images</h1>
+      <ReactJson src={epicList} />
       <Button onClick={onClick}>Push to Users</Button>
     </div>
   );  
 }
+//https://epic.gsfc.nasa.gov/archive/natural/2021/01/14/png/epic_1b_20210114124446.png
