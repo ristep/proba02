5bf70697a869c3b7bbff51021e1fd2765c8e44e4
ristep <ristepan@gmail.com>
2021-02-08 18:13:30 +0100

    about2

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,8 @@ import axios from "axios";
 import { Container } from "react-bootstrap";
 
 const axParam = {
-  baseURL: "https://raw.githubusercontent.com/ristep/proba02/main/ReadMe.md?token=ABCFOIQO7CNTRQ4LHSVOVZDAEEKXQ",
+						// Link to About.md on github repositories
+  baseURL: "https://raw.githubusercontent.com/ristep/proba02/main/About.md",
 	headers: {
 		"Content-type": "application/text"
 	}
@@ -16,7 +17,7 @@ export const About = () =>{
 	const [rdm, setRdm] = useState('Read-me.md');
 	
 	useEffect(() => {
-		axi.get( )
+		axi.get()
     .then((response) => { 
       setRdm(response.data);
     })
