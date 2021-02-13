f77e78e28906cd674728105e8950dd36824c855b
ristep <ristepan@gmail.com>
2021-02-13 01:39:05 +0100

    A5 commit 01

--- src/pages/apod.jsx
+++ src/pages/apod.jsx
@@ -5,7 +5,7 @@ import { useParams, useHistory } from "react-router-dom";
 import moment from "moment";
 import DayPicker from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
-
+//A5 granka
 export const Apod = () => {
   const { year = moment().format('YYYY'), month = moment().format('MM'), day=moment().format('DD') } = useParams();
   const [apod, setApod] = useState({});
